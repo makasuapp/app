197b4951a5a8bcf6dc44d33436b4d0ab1d4bd873
Owen Wang <wangfowen@gmail.com>
2020-09-03 15:08:09 -0400

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'service_locator.dart';
 import 'app.dart';
 
@@ -8,6 +9,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   setupLocator();
+
+  // Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   //throw this in launch.dart initState? how do when there's await?
   await Hive.initFlutter();
